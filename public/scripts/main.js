3c1ed9f6382f43e463a9efa761b4e25dd1226246
Soravis Prakkamakul <asunnotthesun@gmail.com>
2016-07-01 20:52:40 +0700

    Added video grain effect

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,28 +1,29 @@
+/* global $ console */
+
 $(document).ready(function(){
 
   var container, stats;
   var camera, scene, renderer;
   var raycaster;
+  var composer;
   var mouse;
   var PI2 = Math.PI * 2;
-  var programFill = function ( context ) {
+  var programFill = function(context) {
     context.beginPath();
-    context.arc( 0, 0, 0.5, 0, PI2, true );
+    context.arc( 0, 0, 0.5, 0, PI2, true);
     context.fill();
   };
-  var programStroke = function ( context ) {
+  var programStroke = function(context) {
     context.lineWidth = 0.025;
     context.beginPath();
-    context.arc( 0, 0, 0.5, 0, PI2, true );
+    context.arc(0, 0, 0.5, 0, PI2, true);
     context.stroke();
   };
   var INTERSECTED;
-  init();
-  animate();
   function init() {
-    //container = document.createElement( 'div' );
-    //document.body.appendChild( container );
-    var container = document.getElementById( 'threejs' );
+    // container = document.createElement( 'div' );
+    // document.body.appendChild( container );
+    var container = document.getElementById('threejs');
     console.log(container);
 
     // var info = document.createElement( 'div' );
@@ -36,26 +37,28 @@ $(document).ready(function(){
     camera.position.set( 0, 300, 500 );
     scene = new THREE.Scene();
 
+    //scene.fog = new THREE.Fog( 0x000000, 1, 1000 );
+
     var textureLoader = new THREE.TextureLoader();
 
 
     $.ajax({
-      url: 'http://api.randomuser.me/?inc=name,picture&results=1000',
+      url: 'http://api.randomuser.me/?inc=name,picture&results=500',
       dataType: 'json',
       success: function(data){
         console.log(data);
         for(var i in data.results){
           var map = textureLoader.load( data.results[i].picture.thumbnail );
+          //0xFF006C
           var particle = new THREE.Sprite(  new THREE.SpriteMaterial( { map: map, color: 0xffffff, fog: true } ) );
           particle.position.x = Math.random() * 800 - 400;
           particle.position.y = Math.random() * 800 - 400;
           particle.position.z = Math.random() * 800 - 400;
-          particle.scale.x = particle.scale.y = Math.random() * 20 + 20;
+          particle.scale.x = particle.scale.y = Math.random() * 40 + 40;
           scene.add( particle );
         }
       }
     });
-
 
     /*
     for ( var i = 0; i < 100; i ++ ) {
@@ -73,12 +76,26 @@ $(document).ready(function(){
     //
     raycaster = new THREE.Raycaster();
     mouse = new THREE.Vector2();
-    renderer = new THREE.CanvasRenderer();
+    //renderer = new THREE.CanvasRenderer();
+    
+    renderer = new THREE.WebGLRenderer();
+
     renderer.setClearColor( 0xf0f0f0 );
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
     stats = new Stats();
+
+    composer = new THREE.EffectComposer( renderer );
+    composer.addPass( new THREE.RenderPass( scene, camera ) );
+    var effect = new THREE.ShaderPass( THREE.DotScreenShader );
+    effect.uniforms[ 'scale' ].value = 4;
+    composer.addPass( effect );
+    var effect = new THREE.ShaderPass( THREE.RGBShiftShader );
+    effect.uniforms[ 'amount' ].value = 0.0015;
+    effect.renderToScreen = true;
+    composer.addPass( effect );
+
     //container.appendChild( stats.dom );
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
     //
@@ -123,7 +140,10 @@ $(document).ready(function(){
       if ( INTERSECTED ) INTERSECTED.material.program = programStroke;
       INTERSECTED = null;
     }
-    renderer.render( scene, camera );
+    //renderer.render( scene, camera );
+    composer.render();
   }
 
+  init();
+  animate();
 });
