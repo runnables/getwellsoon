7fbb1b51c835b2ab413fa28575084ec9c1c9466b
Sarawoot <sarawoot.piyaarayanunt@gmail.com>
2016-07-01 16:28:51 +0700

    added message collection instead of built in user

--- models/user.js
+++ models/user.js
@@ -6,13 +6,6 @@ const schema = new mongoose.Schema({
   name: { type: String },
   email: { type: String },
   profileImage: { type: String },
-  messages: [
-    {
-      title: { type: String },
-      detail: { type: String },
-      image: { type: String },
-    },
-  ],
 });
 
 module.exports = mongoose.model('User', schema);
