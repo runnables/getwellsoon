6ef799552b0662e27bedcb71c97b8aa7c3f20207
Poomiphat Chamweha <poomipgat@gmail.com>
2016-07-01 18:07:28 +0700

    get message

--- routes/messages.js
+++ routes/messages.js
@@ -7,7 +7,38 @@ const Message = require('../models/message');
 const router = new Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send({ message: 'success' });
+
+	Message.find({ _id : { "$lt" : req.query.lastId }})
+		.limit(30)
+		.populate('user')
+		.sort({ "_id" : -1 })
+		.exec(function(err, messages) {
+		 	if (err) {
+		    	res.status(500).send({ error: err });
+		  	} else {
+		    	res.status(200).send(messages);
+		  	}
+	});
+
+});
+
+router.post('/', (req, res) => {
+
+	var message = new Message({
+		image: req.body.image,
+		title: req.body.title,
+		detail: req.body.detail
+	});
+
+	message.save(function(err){
+		if(err){
+			res.status(500).send({ error: err });
+		} else {
+			res.status(200).send(message);
+		}
+	});
+
+
 });
 
 module.exports = router;
