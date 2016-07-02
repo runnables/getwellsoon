c4b5d98206fd2a33e0fa506c74e88521b37ea07e
Komthep Saksriyuttana <komthep@gmail.com>
2016-07-02 13:16:37 +0700

    fix btnParticipate when open lightbox in mobile

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -303,6 +303,7 @@ $(document).ready(function(){
   };
 
   function showLightbox() {
+    $('.btn-participate').addClass('hidden');
     $('.lightbox-participate').css('display', 'table');
     $('body').css('position', 'fixed');
     $('body').css('top', '0px');
@@ -312,6 +313,7 @@ $(document).ready(function(){
   }
 
   function hideLightbox() {
+    $('.btn-participate').removeClass('hidden');
     $('.lightbox-participate').css('display', 'none');
     $('body').removeAttr('style');
   }
@@ -466,7 +468,7 @@ $(document).ready(function(){
     });
     var msnry = new Masonry( '.grid', { itemSelector: '.grid-item' });
   }
-  
+
 
   // Initialization Code
     // Load the SDK asynchronously
